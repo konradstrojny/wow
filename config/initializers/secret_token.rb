dbb428754bb7e7b185d56192de87c92e8055cc92
Konrad <konrad@copper.io>
2013-12-06 19:14:59 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wow::Application.config.secret_key_base = '5db7fe06db9bc09589a035fbb583eca14971f3050f2e74530ba56dd9b73986f308b067403f1587d1db3896d819a4f710eab4147ff4031cab1fe043f28d407843'
